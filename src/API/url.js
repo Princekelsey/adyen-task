34401eded81c8dd1aa4c9c3852aa5b1eb42bdeec
Prince Ajuobi <princekelsey299@gmail.com>
2020-07-26 20:45:31 +0300

    added https to url

--- src/API/url.js
+++ src/API/url.js
@@ -1,2 +1,2 @@
 export const KIWI_URL = "https://tequila-api.kiwi.com";
-export const WEATHER_URL = "http://dataservice.accuweather.com";
+export const WEATHER_URL = "https://dataservice.accuweather.com";
